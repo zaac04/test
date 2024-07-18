c8c96015bd19d32e5462ce5de4e4e21602d85bbb
Issac <85952413+zaac04@users.noreply.github.com>
2024-07-18 11:21:31 +0530

    Update and rename index.js to lambda_function.py

--- lambda1/index.js
+++
@@ -1,8 +0,0 @@
-// lambda1/index.js
-export async function lambdaHandler(event, context) {
-return {
-  statusCode: 200,
-  body: JSON.stringify({ message: 'Hello from Lambda jadhjdhsabadas' })
-};
-}
- 
